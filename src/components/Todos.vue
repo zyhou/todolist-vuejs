a99cdf708f5f62ba27e510fd62cf6462e96b5631
Maxime <richard.maxime9@gmail.com>
2016-11-05 19:24:29 +0100

    Hello world rapide

---
+++ src/components/Todos.vue
@@ -0,0 +1,13 @@
+<template>
+    <sections class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+        </header>
+    </sections>
+</template>
+
+<script>
+export default {
+    
+}    
+</script>
