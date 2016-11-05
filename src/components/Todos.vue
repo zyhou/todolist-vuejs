b1834a60724c7748bc3676509a66bd01de75687c
Maxime <richard.maxime9@gmail.com>
2016-11-05 19:31:53 +0100

    Début intégration html, reprise de todomvc

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,7 +2,17 @@
     <section class="todoapp">
         <header class="header">
             <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter une tâche" />
         </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo">
+                    <div class="view">
+                        <label>Nom de notre tâche</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
